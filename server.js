bcd372c9e9ffe9718fa0f158699d441c0f604369
KasiaKasia <58384883+KasiaCybula@users.noreply.github.com>
2021-01-19 22:29:29 +0100

    Wersja 1.1

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+const uli = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+  userNewUrlParser: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => console.log("Udało się połączyć z Mongoose."));
