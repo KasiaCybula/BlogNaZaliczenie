9c605109d14592efe0dd6f7e2629bad147b2c88d
KasiaKasia <kasia.cybula@hotmail.com>
2021-01-20 17:45:42 +0100

    Wersja 1.4

--- models/articles.js
+++ models/articles.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -11,17 +10,4 @@ const articleSchema = new Schema({
 const Articles = mongoose.model("Article", articleSchema);
 
 module.exports = Articles;
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-  title: { type: String, required: true },
-  article: { type: String, required: true },
-  authorname: { type: String, required: true },
-});
-
-const Articles = mongoose.model("Article", articleSchema);
-
-module.exports = Articles;
->>>>>>> 24cebef773147e17b503491fe233b883f0eb9cef
