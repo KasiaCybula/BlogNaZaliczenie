7e9310b23f1aad7d6a31a26241b432a24a717164
KasiaKasia <kasia.cybula@hotmail.com>
2021-01-21 16:04:28 +0100

    Wersja 2.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Witaj na naszym blogu :) <br />
+      <button className="btn btn-primary">Kliknij mnie</button>
     </div>
   );
 }
